f85bf4c8f2c2e4c0c64f77b0bfd8e294f7f44aa0
Aydin Pramasta <aydin.pram@gmail.com>
2023-09-03 20:59:29 +0700

    feat: datatable

--- app/Livewire/Users/Index.php
+++ app/Livewire/Users/Index.php
@@ -4,6 +4,8 @@ namespace App\Livewire\Users;
 
 use App\Models\User;
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Builder;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -11,9 +13,51 @@ class Index extends Component
 {
     use WithPagination;
 
+    #[Url(as: 'q', history: true)]
+    public string $query = '';
+
+    #[Url(history: true)]
+    public int $limit = 10;
+
+    #[Url()]
+    public string $field = 'created_at';
+
+    #[Url()]
+    public string $direction = 'desc';
+
+    public function sortBy(string $column): void
+    {
+        if ($this->field === $column) {
+            $this->direction = $this->direction === 'asc' ? 'desc' : 'asc';
+        } else {
+            $this->direction = 'asc';
+        }
+
+        $this->field = $column;
+    }
+
+    public function updated(mixed $property): void
+    {
+        if ($property === 'query') {
+            $this->resetPage();
+        }
+    }
+
     public function render(): View
     {
-        $users = User::query()->paginate(10);
+        $users = User::query()
+            ->when($this->query, function (Builder $query) {
+                $query->where('name', 'LIKE', '%' . $this->query . '%')
+                    ->orWhere('email', 'LIKE', '%' . $this->query . '%');
+            })->when(
+                $this->field && $this->direction,
+                function (Builder $query) {
+                    $query->orderBy($this->field, $this->direction);
+                },
+                function (Builder $query) {
+                    $query->latest();
+                },
+            )->paginate($this->limit);
 
         return view('livewire.users.index')
             ->with(['users' => $users])
--- resources/views/livewire/users/index.blade.php
+++ resources/views/livewire/users/index.blade.php
@@ -3,14 +3,50 @@
         <div class="card-body">
             <h2 class="card-title mb-4">List Users</h2>
 
+            <div class="row mb-4">
+                <div class="col-md-3">
+                    <label for="search" class="visually-hidden">Search...</label>
+                    <input wire:model.live.debounce.500ms="query"
+                           type="search" id="search"
+                           class="form-control" placeholder="Search..."/>
+                </div>
+
+                <div class="col-md-1">
+                    <label for="limit" class="visually-hidden">Limit</label>
+                    <select wire:model.live="limit"
+                            id="limit" class="form-select">
+                        <option value="10">10</option>
+                        <option value="25">25</option>
+                        <option value="50">50</option>
+                        <option value="75">75</option>
+                        <option value="100">100</option>
+                    </select>
+                </div>
+            </div>
+
             <div class="mw-100 overflow-auto">
                 <table class="table table-hover">
                     <thead>
                     <tr>
                         <th scope="col">#</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Created At</th>
+                        <th scope="col">
+                            <span role="button" wire:click="sortBy('name')">
+                                Name
+                                <x-sort-icon column="name" :field="$field" :direction="$direction"/>
+                            </span>
+                        </th>
+                        <th scope="col">
+                            <span role="button" wire:click="sortBy('email')">
+                                Email
+                                <x-sort-icon column="email" :field="$field" :direction="$direction"/>
+                            </span>
+                        </th>
+                        <th scope="col">
+                            <span role="button" wire:click="sortBy('created_at')">
+                                Created At
+                                <x-sort-icon column="created_at" :field="$field" :direction="$direction"/>
+                            </span>
+                        </th>
                         <th scope="col">Action</th>
                     </tr>
                     </thead>
